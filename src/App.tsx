94817498b30732bc04c56dd301a6311ad772639e
SadiqNaizam <sadiq.naizam1234@gmail.com>
2025-06-26 10:53:00 +0530

    Updated code files via regeneration: App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,32 +3,34 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
+import { useEffect } from "react";
 
 import AnimatorWorkspace from "./pages/AnimatorWorkspace";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
 
+const App = () => {
+  useEffect(() => {
+    // Apply dark mode by default to the entire application
+    document.documentElement.classList.add('dark');
+  }, []);
 
-const App = () => (
-<QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-    <Toaster />
-    <Sonner />
-    <BrowserRouter>
-        <Routes>
-
-
-          <Route path="/" element={<AnimatorWorkspace />} />
-          {/* catch-all */}
-          <Route path="*" element={<NotFound />} />
-
-
-        </Routes>
-    </BrowserRouter>
-    </TooltipProvider>
-</QueryClientProvider>
-);
+  return (
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Toaster />
+        <Sonner />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<AnimatorWorkspace />} />
+            {/* catch-all */}
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </TooltipProvider>
+    </QueryClientProvider>
+  );
+};
 
 export default App;
